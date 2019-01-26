7cf153daae7b2ba7a6a93d0edfe76a10286e7ed2
Jack Zhang <ngshoy@hotmail.com>
2019-01-26 14:51:20 -0500

    use v-for for poll options instead, improve ux

--- src/views/CreatePoll.vue
+++ src/views/CreatePoll.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
     <header>Create Your Personalized Poll</header>
-    <label for="pollTopic">What's your topic?</label>
-    <input type="text" id="pollTopic" name="pollTopic" v-model="poll.pollTopic">
     <br>
-    <label for="pollOption">Add your options</label>
-    <input type="text" id="pollOption" v-model="pollOptionInput">
-    <button v-on:click="addPollOption">add option</button>
+    <label for="pollTopic">What is your topic?</label>
+    <div>
+      <input type="text" id="pollTopic" name="pollTopic" v-model="poll.pollTopic">
+    </div>
+    <br>
+    <label for="pollOptions">What are the options?</label>
+    <ul name="pollOptions">
+      <li v-for="(option, index) in poll.pollOptions" v-bind:key="index">
+        <input type="text" v-model="poll.pollOptions[index]">
+      </li>
+    </ul>
+    <button v-on:click="addPollOption">add another option</button>
     <br>
     ---------------------------------
 
@@ -17,21 +24,26 @@
 </template>
 
 <script>
-export default {
-  name: 'create-poll',
-  data: () => ({
-    poll: {
-      pollTopic: '',
-      pollOptions: [],
+  export default {
+    name: 'create-poll',
+    data: () => ({
+      poll: {
+        pollTopic: '',
+        pollOptions: [''],
+      },
+    }),
+    methods: {
+      addPollOption() {
+        this.poll.pollOptions.push('');
+      },
     },
-    pollOptionInput: '',
-  }),
-  methods: {
-    addPollOption() {
-      this.poll.pollOptions.push(this.pollOptionInput);
-      this.pollOptionInput = '';
-    },
-  },
-};
-
+  };
 </script>
+
+<style scoped>
+  ul {
+    padding: 0;
+    margin: 0;
+    list-style-type: none;
+  }
+</style>
