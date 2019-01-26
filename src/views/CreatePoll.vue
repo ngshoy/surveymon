70ec2862320da062d30a12b3c8e717d07c47f552
Jack Zhang <ngshoy@hotmail.com>
2019-01-25 22:12:43 -0500

    enhanced create poll page

--- src/views/CreatePoll.vue
+++ src/views/CreatePoll.vue
@@ -1,9 +1,37 @@
 <template>
-    <p>this is a page to create polls</p>
+  <div>
+    <header>Create Your Personalized Poll</header>
+    <label for="pollTopic">What's your topic?</label>
+    <input type="text" id="pollTopic" name="pollTopic" v-model="poll.pollTopic">
+    <br>
+    <label for="pollOption">Add your options</label>
+    <input type="text" id="pollOption" v-model="pollOptionInput">
+    <button v-on:click="addPollOption">add option</button>
+    <br>
+    ---------------------------------
+
+    <header>Preview Your Poll</header>
+    <p>{{poll.pollTopic}}</p>
+    <p>{{poll.pollOptions}}</p>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'create-poll',
+  data: () => ({
+    poll: {
+      pollTopic: '',
+      pollOptions: [],
+    },
+    pollOptionInput: '',
+  }),
+  methods: {
+    addPollOption() {
+      this.poll.pollOptions.push(this.pollOptionInput);
+      this.pollOptionInput = '';
+    },
+  },
 };
+
 </script>
