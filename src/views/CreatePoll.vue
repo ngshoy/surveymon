20242e98e7d26308eafc49285033ab12a221dfff
Jack Zhang <ngshoy@hotmail.com>
2019-02-03 13:48:28 -0500

    converted preview poll section to vuetify

--- src/views/CreatePoll.vue
+++ src/views/CreatePoll.vue
@@ -32,56 +32,30 @@
               PREVIEW YOUR POLL
             </div>
           </v-card-title>
+          <v-card-text>
+            <v-subheader>{{poll.topic}}</v-subheader>
+            <v-list :dark=false>
+              <v-list-tile v-for="(option, index) in poll.options" :key="index">
+                <v-layout justify-space-between align-center>
+                  <span>{{index + 1}}. {{option.name}}</span>
+                  <v-btn flat icon @click="removePollOption(index)">
+                    <v-icon>clear</v-icon>
+                  </v-btn>
+                </v-layout>
+              </v-list-tile>
+            </v-list>
+          </v-card-text>
+          <v-card-actions>
+            <v-layout justify-end>
+              <v-btn flat @click="createNewPoll">Create poll</v-btn>
+            </v-layout>
+          </v-card-actions>
         </v-card>
       </v-flex>
     </v-layout>
   </v-container>
   <!-- <div class="md-layout">
     <div class="md-layout-item">
-      <md-card>
-        <md-card-header>
-          <div class="md-title">Create Your Personalized Poll</div>
-        </md-card-header>
-        <md-card-content>
-          <md-field>
-            <label>What is your topic?</label>
-            <md-input type="text" v-model="poll.topic"></md-input>
-          </md-field>
-          <label for="options">What are the options?</label>
-          <ul name="options">
-            <li v-for="(option, index) in poll.options" v-bind:key="index">
-              <md-field md-inline>
-                <label>Input Option</label>
-                <md-input type="text" v-model="poll.options[index].name"></md-input>
-              </md-field>
-            </li>
-          </ul>
-        </md-card-content>
-        <md-card-actions>
-          <md-button class="md-accent" @click="addPollOption">Add another option</md-button>
-        </md-card-actions>
-      </md-card>
-    </div>
-    <div class="md-layout-item">
-      <md-card>
-        <md-card-header>
-          <div class="md-title">Preview Your Poll</div>
-        </md-card-header>
-        <md-card-content>
-          <md-subheader>{{poll.topic}}</md-subheader>
-          <md-list>
-            <md-list-item v-for="(option, index) in poll.options" v-bind:key="index">
-              <span class="md-list-item-text">{{index + 1}}. {{option.name}}</span>
-              <md-button class="md-icon-button md-list-action" @click="removePollOption(index)">
-                <md-icon class="md-primary">clear</md-icon>
-              </md-button>
-            </md-list-item>
-          </md-list>
-        </md-card-content>
-        <md-card-actions>
-          <md-button class="md-primary" @click="createNewPoll">Create your poll</md-button>
-        </md-card-actions>
-      </md-card>
 
       <md-dialog :md-active.sync="showDialog">
         <md-dialog-title v-html="respMsg"></md-dialog-title>
@@ -162,17 +136,8 @@ export default {
     list-style-type: none;
   }
 
-  ul li input {
-    width: 100px;
-  }
-
-  #topic {
-    width: 200px;
-  }
-
-  .md-card {
-    width: 460px;
-    margin: 10px;
+  .theme--dark.v-list {
+    background: transparent;
   }
 
 </style>
