353fd5343f06f07f09b15d669c165f6b1abb206d
Jack Zhang <ngshoy@hotmail.com>
2019-02-03 21:42:30 -0500

    integrated View Poll page with vuetify

--- src/views/ViewPoll.vue
+++ src/views/ViewPoll.vue
@@ -1,10 +1,29 @@
 <template>
-  <div>
-    <p>this is a page to vote and view on poll {{pollData._id}}</p>
-    <ul>
-      <li v-for="option in pollData.options" v-bind:key="option">{{option}}</li>
-    </ul>
-  </div>
+  <v-container grid-list-lg>
+    <v-layout row wrap justify-space-around>
+      <v-flex md8>
+        <v-card color="primary">
+          <v-card-title primary-title class="justify-center">
+            <div class="headline">
+              {{pollData.topic}}
+            </div>
+          </v-card-title>
+          <v-card-text>
+            <v-radio-group v-model="selectedOption">
+              <v-radio v-for="(option, index) in pollData.options" :key="index"
+                :label="option.name" :value="option.name"
+                on-icon="radio_button_checked" off-icon="radio_button_unchecked"/>
+            </v-radio-group>
+          </v-card-text>
+          <v-card-actions>
+            <v-layout justify-end>
+              <v-btn flat @click="upvote">Vote</v-btn>
+            </v-layout>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -16,8 +35,8 @@ export default {
     id: String,
   },
   data: () => ({
-    loading: false,
     pollData: {},
+    selectedOption: '',
   }),
   created() {
     this.fetchData();
@@ -27,7 +46,6 @@ export default {
   },
   methods: {
     fetchData() {
-      this.loading = true;
       axios.get(`http://localhost:3000/ViewPoll/${this.id}`)
         .then((res) => {
           this.pollData = res.data;
@@ -35,8 +53,17 @@ export default {
         .catch((err) => {
           console.error(err);
         });
-      this.loading = false;
+    },
+    upvote() {
+      console.log(`upvoted ${this.selectedOption}`);
     },
   },
 };
+
 </script>
+
+<style lang="scss" scoped>
+  .theme--dark.v-label {
+    color: inherit;
+  }
+</style>
