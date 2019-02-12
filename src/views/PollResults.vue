4c0cceaf8215519e77f6a7df93b76d40c65fa6ec
Jack Zhang <ngshoy@hotmail.com>
2019-02-11 22:14:36 -0500

    add data processing, TODO: refactor chart out into own component

--- src/views/PollResults.vue
+++ src/views/PollResults.vue
@@ -1,22 +1,41 @@
 <template>
   <v-container grid-list-lg>
-    You're viewing {{pollData._id}}
+    You're viewing {{pollChartData._id}}
   </v-container>
 </template>
 
 <script>
 import axios from 'axios';
+import { Bar } from 'vue-chartjs';
 
 export default {
+  extends: Bar,
   name: 'poll-results',
   props: {
     id: String,
   },
   data: () => ({
-    pollData: {},
+    pollData: null,
+    pollChartData: {
+      labels: [],
+      datasets: [
+        {
+          label: 'Vote Results',
+          backgroundColor: '#f87979',
+          data: [],
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+    },
   }),
   created() {
     this.fetchData();
+  },
+  mounted() {
+    this.renderChart(this.pollChartData, this.options);
   },
   watch: {
     $route: 'fetchData',
@@ -26,6 +45,10 @@ export default {
       axios.get(`http://localhost:3000/ViewPoll/${this.id}`)
         .then((res) => {
           this.pollData = res.data;
+          res.data.options.forEach((option) => {
+            this.pollChartData.labels.push(option.name);
+            this.pollChartData.datasets[0].data.push(option.voteCount);
+          });
         })
         .catch((err) => {
           console.error(err);
