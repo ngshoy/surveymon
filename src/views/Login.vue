59c2e21e0c4a18d7d86a29e4365b54a8f3f943ed
Jack Zhang <ngshoy@hotmail.com>
2019-02-21 22:32:08 -0500

    added card and style to login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,29 @@
 <template>
-  <div>This is a login page</div>
+  <v-container grid-list-lg>
+    <v-layout row wrap align-center justify-center fill-height>
+      <v-flex md6>
+        <v-card color="primary">
+          <v-card-title primary-title class="justify-center">
+            <div class="headline">
+              LOGIN
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+
 </template>
 
 <script>
 export default {
   name: 'login',
-}
+};
+
 </script>
+
+<style lang="scss" scoped>
+  .container {
+    height: 100vh;
+  }
+</style>
