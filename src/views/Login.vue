961b038b833ed7505adf68668a0062f9a27a6195
Jack Zhang <ngshoy@hotmail.com>
2019-02-22 21:27:27 -0500

    complete UI for login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,11 +3,25 @@
     <v-layout row wrap align-center justify-center fill-height>
       <v-flex md6>
         <v-card color="primary">
-          <v-card-title primary-title class="justify-center">
+          <v-card-title primary-title>
             <div class="headline">
-              LOGIN
+              Welcome
             </div>
           </v-card-title>
+          <v-card-text>
+            <v-text-field label="user name" type="text" color="tertiary" v-model="login.userName" />
+            <v-text-field label="password" type="password"
+            color="tertiary" v-model="login.password" />
+            <!-- :append-icon="showPassword ? 'visibility_off' : 'visibility'"
+            :type="showPassword ? 'text' : 'password'"
+            @click:append="showPassword = !showPassword"/> -->
+          </v-card-text>
+          <v-card-actions>
+            <v-layout justify-end>
+              <v-btn flat to="register">Register</v-btn>
+              <v-btn flat>Login</v-btn>
+            </v-layout>
+          </v-card-actions>
         </v-card>
       </v-flex>
     </v-layout>
@@ -18,6 +32,13 @@
 <script>
 export default {
   name: 'login',
+  data: () => ({
+    login: {
+      userName: '',
+      password: '',
+    },
+    // showPassword: false,
+  }),
 };
 
 </script>
